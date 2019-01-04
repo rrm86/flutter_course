00f1a30b60899777574ed4bac3dbd00f98d8a898
Ronnald R Machado <rrmachado@id.uff.br>
2019-01-04 14:50:33 -0200

    Stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,16 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+
+}
+class _MyAppState extends State<MyApp>{
+   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
@@ -17,7 +25,9 @@ class MyApp extends StatelessWidget {
               Container(
                 margin:EdgeInsets.all(10.0),
                 child: RaisedButton(
-                  onPressed: () {},
+                  onPressed: () {
+
+                  },
                   child: Text('Add Picture'),
                 ),
               ),
@@ -33,4 +43,7 @@ class MyApp extends StatelessWidget {
           )),
     );
   }
+
 }
+ 
+
